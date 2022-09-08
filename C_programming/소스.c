9e010b490f3413b03c077b4bd7143a6a1d2e205f
somgam6373 <somgam6373@gmail.com>
2022-09-08 13:24:55 +0900

    초기 커밋.

---
+++ C_programming/소스.c
@@ -0,0 +1,33 @@
+
+
+/*
+#include<stdio.h>
+
+int main(void)
+{
+	printf("홍길동 \n홍 길 동 \n홍  길  동");
+	return 0;
+}
+*/
+
+/*
+#include<stdio.h>
+int main(void)
+
+{
+	printf("My name: %s \n", "Lee Junhyuk");
+	printf("My address: %s \n", "경기도 남양주시 진접읍 해밀예당3로 38");
+	printf("My phone number: %s \n", "01063739763");
+	return 0;
+}
+*/
+
+#include<stdio.h>
+int main(void)
+
+{
+	printf("C프로그래밍 11주차 과제 \n");
+	printf("이름: %s \n", "이준혁");
+	printf("학번: %s \n", "202210962");
+	return 0;
+}
