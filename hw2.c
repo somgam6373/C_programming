a4ca37a4e4b373fe0df785e747a4710af8435d6d
somgam6373 <105191945+somgam6373@users.noreply.github.com>
2022-09-23 13:49:11 +0900

    Add files via upload

---
+++ hw2.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main(void)
+
+{
+	double km;
+	double mile;
+
+	printf("please enter kilometers:");
+	scanf("%lf", &km);
+
+	mile = km / 1.609;
+	printf("%.1f km is equal to %.1f miles \n",km,mile);
+	return 0;
+
+
+}
+
+
